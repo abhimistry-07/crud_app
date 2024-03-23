c48949ee64df3387d5965c58b54cec5a3e8da3f7
Abhishek <mistryabhishek2000@gmail.com>
2024-03-23 20:51:53 +0530

    Add new product part added

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { environment } from 'src/environment';
 
 @Component({
   selector: 'app-root',
@@ -7,19 +9,36 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private http: HttpClient) { }
 
-  addProductForm = new FormGroup({
-    productName: new FormControl(''),
-    discription: new FormControl(''),
-    price: new FormControl(''),
-  });
-
-
-  onSubmitForm() {
-    console.log(this.addProductForm.value);
-  }
+  url: string = '';
 
   ngOnInit() {
+    this.url = environment.apiUrl;
+  }
+
+  addProductForm = new FormGroup({
+    productName: new FormControl('Product 1'),
+    discription: new FormControl('Discription 1'),
+    price: new FormControl(0),
+  });
+
+  onSubmitForm() {
+    this.addNewProduct();
+    // console.log(this.addProductForm.value);
+  }
+
+
+  addNewProduct() {
+    let product = this.addProductForm.value;
+
+    this.http.post<any>(`${this.url}/evaluation.json`, product).subscribe((response) => {
+      alert('Product added successfully!');
+      this.addProductForm.reset();
+    }, (error) => {
+      console.log(error);
+      alert('Something went wrong!')
+    })
   }
 
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
@@ -14,6 +16,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
